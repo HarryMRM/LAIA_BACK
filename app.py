711df6cea0167a6270def510cdd3be3063708766
Abraham López Chacón <abraham.lopez.chacon@uabc.edu.mx>
2024-10-06 21:07:15 -0700

    comentario

--- app.py
+++ app.py
@@ -1,6 +1,17 @@
+#Se importan las respectivas clases 
+#Primero se importa la clase Flask para poder trabajar en python utilizando Flask.
+#Posteriormente se utiliza cors el cual permite trabajar con el mismo, otorgando permisos de acceso para la aplicación segun sea necesario.
+#Finalmente la clase text_to_speech la cual se encarga de convertir el texto en audio.
+
 from flask import Flask
 from flask_cors import CORS
 from routes.text_to_speech import text_to_speech_route
+
+#Se crea la aplicación flask y se habilita CORS para otorgar los permisos necesarios que sean requeridos por
+# los dominios del front end. 
+#Se registra un blueprint para estructurar las operaciones a ejecutar en la aplicación desde text_to_speech.
+#Se verifica que el archivo este siendo ejecutado directamente y en caso de cumplirse tal condición,
+# se core la aplicación flask en modo depuración, permitiendo constante actualización durante su ejecución.
 
 app = Flask(__name__)
 CORS(app)
