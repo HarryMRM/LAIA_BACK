14e18929dbc373c7b9d75deea54bd01411e375ee
Abraham López Chacón <abraham.lopez.chacon@uabc.edu.mx>
2024-10-06 22:34:58 -0700

    comentario

--- routes/text_to_speech.py
+++ routes/text_to_speech.py
@@ -1,8 +1,19 @@
+#Se importa blueprint para organizar el código, accede a los datos http y los convierte en formato JSON.
+#Se importa la función para convertir audio en texto mediante openai y la función para dar formato base64 a los archivos.
 from flask import Blueprint, request, jsonify
 from services.openai_client import generate_speech
 from utils.file_utils import convert_file_to_base64
 text_to_speech_route = Blueprint('text_to_speech', __name__)
 
+#Se crea un blueprint para las rutas de texto y voz.
+# se define la ruta que acepta solo solicitudes POST.
+# Se obtiene el contenido JSON de la solicitud la cual deberia contener "input_text".
+# Posteriormente se verifica que este presente,  si no esta presente enviara un mensaje de error,
+# de caso contrario, se extrae el texto y se llama la función para generar el audio a partir del mismo.
+# El archivo de audio se pasa a formato base64 y una vez terminado el proceso.
+# Se crea y retorna JSON, el texto, la ruta del archivo de audio y la version Base64.
+# Se utiliza except en caso de que ocurra un error dentro de try, regresando un mensaje de error en formato JSON,
+# junto con el codigo 500, error interno del servidor. 
 @text_to_speech_route.route('/api/text-to-speech', methods=['POST'])
 def text_to_speech():
     try:
--- services/openai_client.py
+++ services/openai_client.py
@@ -1,8 +1,17 @@
+#Primero se importa la libreria openai la cual proporciona acceso a las funcionalidades de OPENIA.
+#Despues se importa la clase path que ayuda con las rutas de archivos entre sistemas operativos.
+#Se importa OPENAI_API_KEY la cual permite autentificar las solicitudes de OPENAI.
 import openai
 from pathlib import Path
 from config import OPENAI_API_KEY
 openai.api_key = OPENAI_API_KEY
 
+#Se define la llamada que va a recibir el texto a convertir en audio como parametro.
+#Despues se crea un cliente para realizar solicitudes a la API de OpenIA.
+#Posteriormente se define la ruta del archivo donde se guardara la conversión a voz del texto
+# Se realiza la llamada para crear el audio mediante OPENAI, se selecciona el modelo de voz,
+# la tonalidad de la misma y el texto que se utilizará.
+# Finalmente se guarda en la ruta el archivo de voz y la retorna para su uso posterior. 
 def generate_speech(input_text):
     client = openai.OpenAI()
     speech_file_path = Path('speech-result.mp3')
