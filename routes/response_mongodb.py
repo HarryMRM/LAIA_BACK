1a950e1b94547f03d559f588bf925f4c3336acad
Abraham-Niebla <a1176033@uabc.edu.mx>
2024-10-26 21:10:00 -0700

    Manejo de base de datos MongoDB

--- routes/response_mongodb.py
+++ routes/response_mongodb.py
@@ -1,14 +1,17 @@
 """Se importa blueprint para organizar el código, accede a los datos http y los convierte en formato JSON.
+Despues se importa la clase datetime que ayuda con la marca de tiempo a la hora de insertar un nuevo elemento en la base de datos.
 Se importan las funciones para insertar un documento el la base de datos, leer todos los documentos de la base de datos,
 y eliminar todos los documentos  de la base de datos."""
 from flask import Blueprint, request, jsonify
+from datetime import datetime
 from services.mongodb import insertDocument, readDocuments, deleteAllDocuments
 response_mongodb_route = Blueprint('messages_database', __name__)
+import time
 
 """
 Se crea un blueprint para las rutas de la base de datos.
 Se define la ruta que acepta solo solicitudes POST para la base de datos.
-Se obtiene el contenido JSON de la solicitud la cual deberia contener "userID", "message" y "photoUrl".
+Se obtiene el contenido JSON de la solicitud la cual deberia contener "uid", "message" y "photoUrl".
 Posteriormente se verifica que estos esten presentes, si no estan presentes enviara un mensaje de error de petición,
 en caso contrario, se extraen los datos y se llama la función para insertar un documento en la base de datos.
 La función de insersión recibe un diccionario con los datos a insertar y retorna el objeto insertado.
@@ -21,13 +24,19 @@ def insert_message():
   try:
     data = request.get_json()
     
-    if ('userID' not in data) or ('message' not in data) or ('photoUrl' not in data):
-      return jsonify({"error": "You may need to add a userID, message or photoUrl."}), 400
+    if ('uid' not in data) or ('message' not in data) or ('photoURL' not in data):
+      return jsonify({"error": "You may need to add a uid, message or photoURL."}), 400
     
+    if ('createdAt' not in data):
+      date = datetime.now()
+    else:
+      date = data.get('createdAt')
+      
     document = {
-      "userID": data.get('userID'),
+      "uid": data.get('uid'),
       "message": data.get('message'),
-      "photoUrl": data.get('photoUrl'),
+      "photoURL": data.get('photoURL'),
+      "createdAt": date
     }
     inserted = insertDocument(document)
     return jsonify(inserted), 200
@@ -70,8 +79,8 @@ junto con el codigo 500, error interno del servidor.
 @response_mongodb_route.route('/api/messages_database', methods=['DELETE'])
 def delete_messages():
   try:
-    deleteAllDocuments()
-    return jsonify({"message": "All messages deleted successfully"}), 200
+    result = deleteAllDocuments()
+    return jsonify({"message": f"{result} messages deleted successfully"}), 200
   
   except Exception as e:
     return jsonify({"error deleting": str(e)}), 500
--- services/mongodb.py
+++ services/mongodb.py
@@ -1,29 +1,27 @@
 """Primero se importa la pymongo openai la cual proporciona acceso a las funcionalidades de MongoDB.
-Despues se importa la clase datetime que ayuda con la marca de tiempo a la hora de insertar un nuevo elemento en la base de datos.
 Se importan MONGO_URI, MONGO_DATABASE, y MONGO_COLLECTION las cuales permiten autentificar las solicitudes a MongoDB."""
 import pymongo
-from datetime import datetime
 from config import MONGO_URI, MONGO_DATABASE, MONGO_COLLECTION
 
 """ Se establece un tiempo de espera límite de 1000 milisegundos (1 segundo) para la conexión con la base de datos """
 TIMEOUT = 1000
-
 
 """
 Se define una función la cual se encarga de conectar con el cliente de la base de datos
 No recibe parámetros
 Retorna la conexión con el cliente
 """
-def connectClient():
-  try:
-    cliente = pymongo.MongoClient(MONGO_URI, serverSelectionTimeoutMS=TIMEOUT)
-    return cliente
+# def connectClient():
+try:
+  cliente = pymongo.MongoClient(MONGO_URI, serverSelectionTimeoutMS = TIMEOUT)
+  print("Conexión con la base de datos establecida!")
+  # return cliente
+
+except pymongo.errors.ServerSelectionTimeoutError as timeError:
+  print(f"Tiempo excedido: {timeError}")
   
-  except pymongo.errors.ServerSelectionTimeoutError as timeError:
-    print(f"Tiempo excedido: {timeError}")
-    
-  except pymongo.errors.OperationFailure as operationError:
-    print(f"Error de operación: {operationError}")
+except pymongo.errors.OperationFailure as operationError:
+  print(f"Error de operación: {operationError}")
 
 
 """
@@ -31,14 +29,14 @@ Se define una función la cual se encarga de conectar con la colección de la ba
 Recibe la conexión con el cliente
 Retorna la conexión con la colección
 """
-def connectCollection(cliente):
-  try:
-    db = cliente[MONGO_DATABASE]
-    collection = db[MONGO_COLLECTION]
-    return collection
-    
-  except pymongo.errors.CollectionInvalid as collectionError:
-    print(f"Error al conectar con la colección: {collectionError}")
+# def connectCollection(cliente):
+try:
+  db = cliente[MONGO_DATABASE]
+  collection = db[MONGO_COLLECTION]
+  # return collection
+  
+except pymongo.errors.CollectionInvalid as collectionError:
+  print(f"Error al conectar con la colección: {collectionError}")
 
 
 """
@@ -46,11 +44,11 @@ Se define una función la cual se encarga de cerrar la conexión con el cliente 
 Recibe la conexión con el cliente
 No tiene retorno
 """
-def closeClient(cliente):
-  try:
-    cliente.close()
-  except Exception as error:
-    print(f"Error al cerrar la conexión: {error}")
+# def closeClient(cliente):
+#   try:
+#     cliente.close()
+#   except Exception as error:
+#     print(f"Error al cerrar la conexión: {error}")
 
 
 """
@@ -60,22 +58,10 @@ Retorna el objeto insertado
 """
 def insertDocument(doc):
   try:
-    cliente = connectClient()
-    collection = connectCollection(cliente)
-    
-    document = {
-      "userID": doc.get('userID'),
-      "message": doc.get('message'),
-      "photoUrl": doc.get('photoUrl'),
-      "createdAt": datetime.now()
-    } #Clase "dict" (un diccionario Python)
-    
-    resp = collection.insert_one(document)
+    resp = collection.insert_one(doc)
     
     inserted = collection.find_one({"_id": resp.inserted_id})
     inserted["_id"] = str(inserted.get("_id"))
-    
-    closeClient(cliente)
     
     return inserted
     
@@ -90,14 +76,9 @@ Retorna una lista con los objetos presentes en la base de datos
 """
 def readDocuments():
   try:
-    cliente = connectClient()
-    collection = connectCollection(cliente)
-
     documents = list(collection.find().sort("createdAt", pymongo.ASCENDING))
     for document in documents:
       document["_id"] = str(document.get("_id"))
-    
-    closeClient(cliente)
     
     return documents
   
@@ -112,12 +93,8 @@ No tiene retorno
 """
 def deleteAllDocuments():
   try:
-    cliente = connectClient()
-    collection = connectCollection(cliente)
-    
-    collection.delete_many(filter={})
-    
-    closeClient(cliente)
+    deleteResult = collection.delete_many(filter={})
+    return deleteResult.deleted_count
     
   except pymongo.errors.ConnectionFailure as connectionError:
     print(f"Error al borrar el documento: {connectionError}")
