fa9c051ec1c630efaff13866c275ffe1694bc668
Abraham-Niebla <a1176033@uabc.edu.mx>
2024-11-11 22:16:15 -0800

    Módulo incompleto

--- services/users_admin.py
+++ services/users_admin.py
@@ -32,18 +32,72 @@ except pymongo.errors.CollectionInvalid as collectionError:
 
 
 """
+Se define una función la cual se encarga de validar si un usuario ya existe en la base de datos
+"""
+def user_exists(doc):
+  try:
+    user = collection.find_one({"user": doc.get("user")})
+    
+    if ('_id' in user):
+      return True #If the user does exist
+    else:
+      return False #If the user doesn't exists
+  
+  except pymongo.errors.ConnectionFailure as connectionError:
+    print(f"Error al insertar el documento: {connectionError}")
+
+
+"""
 Se define una función la cual se encarga de insertar un documento en la colección
 Recibe un diccionario con los datos a insertar
 Retorna el objeto insertado
 """
 def insertDocument(doc):
   try:
-    resp = collection.insert_one(doc)
+    if not user_exists(doc): #If the user doesn't exist
+      resp = collection.insert_one(doc)
+      
+      inserted = collection.find_one({"_id": resp.inserted_id})
+      inserted["_id"] = str(inserted.get("_id"))
+      
+      return inserted
     
-    inserted = collection.find_one({"_id": resp.inserted_id})
-    inserted["_id"] = str(inserted.get("_id"))
-    
-    return inserted
+    else: #If the user do exists
+      return {"error": "User already exists"}
     
   except pymongo.errors.ConnectionFailure as connectionError:
     print(f"Error al insertar el documento: {connectionError}")
+
+
+"""
+Se define una función la cual se encarga de leer todos los documentos de la colección
+No recibe parámetros
+Retorna una lista con los objetos presentes en la base de datos
+"""
+def validate_user(doc):
+  try:
+  
+    user = collection.find_one({"user": doc.get("user")})
+    
+    documents = list(collection.find().sort("createdAt", pymongo.ASCENDING))
+    for document in documents:
+      document["_id"] = str(document.get("_id"))
+    
+    return documents
+  
+  except pymongo.errors.ConnectionFailure as connectionError:
+    print(f"Error al leer los documentos: {connectionError}")
+
+
+"""
+Se define una función la cual se encarga de eliminar todos los documentos de la colección
+No recibe parámetros
+No tiene retorno
+"""
+def deleteAllDocuments():
+  try:
+    deleteResult = collection.delete_many(filter={})
+    return deleteResult.deleted_count
+    
+  except pymongo.errors.ConnectionFailure as connectionError:
+    print(f"Error al borrar el documento: {connectionError}")
