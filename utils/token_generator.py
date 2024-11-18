49b3ac5d766b9f7912244c2430511e4f6621e31b
Abraham-Niebla <a1176033@uabc.edu.mx>
2024-11-17 22:09:07 -0800

    Formato y cambio de expiración

--- utils/token_generator.py
+++ utils/token_generator.py
@@ -1,4 +1,4 @@
-import jwt # type: ignore
+import jwt  # type: ignore
 import datetime
 
 """
@@ -6,11 +6,17 @@ Se define una función la cual se encarga de crear una firma jwt
 Recibe una carga útil
 Retorna un token o un error
 """
-def sign(payload, is_access_token): #secret es la clave secreta
-    expiration = datetime.datetime.now(datetime.timezone.utc) + datetime.timedelta(seconds=10)  # Token expira en 10 segundos
-    return jwt.encode({"some": payload, "exp": expiration},
-                      "access" if is_access_token else "refresh",
-                      algorithm="HS256")
+
+
+def sign(payload, is_access_token):  # secret es la clave secreta
+    expiration = datetime.datetime.now(datetime.timezone.utc) + datetime.timedelta(
+        seconds=3600
+    )  # Token expira en 3600 segundos (60 minutos o 1 hora)
+    return jwt.encode(
+        {"some": payload, "exp": expiration},
+        "access" if is_access_token else "refresh",
+        algorithm="HS256",
+    )
 
 
 """
@@ -18,12 +24,16 @@ Se define una función la cual se encarga de crear una firma jwt
 Recibe el token a decodificar
 Retorna la carga tokenizada o un error de expiración
 """
-def decode(encoded_jwt, is_access_token): #secret es la clave secreta
+
+
+def decode(encoded_jwt, is_access_token):  # secret es la clave secreta
     try:
-        return jwt.decode(encoded_jwt,
-                          "access" if is_access_token else "refresh",
-                          algorithms=["HS256"])
-    
+        return jwt.decode(
+            encoded_jwt,
+            "access" if is_access_token else "refresh",
+            algorithms=["HS256"],
+        )
+
     except jwt.ExpiredSignatureError:
         return "Token has expired"
 
@@ -33,13 +43,18 @@ Se define una función la cual se encarga de crear un token de acceso
 Recibe 
 Retorna un token o un error
 """
+
+
 def create_access_token(doc):
     return sign(doc, True)
+
 
 """
 Se define una función la cual se encarga de crear un token de recarga
 Recibe 
 Retorna un token o un error
 """
+
+
 def create_refresh_token(doc):
     return sign(doc, False)
