d688b49910099ed629bca02cf46402286beacef4
Abraham-Niebla <a1176033@uabc.edu.mx>
2024-11-17 21:34:57 -0800

    Modulo de tokens JWT casi terminado

--- utils/token_generator.py
+++ utils/token_generator.py
@@ -1,23 +1,31 @@
-import jwt
+import jwt # type: ignore
+import datetime
+
+"""
+Se define una función la cual se encarga de crear una firma jwt
+Recibe una carga útil
+Retorna un token o un error
+"""
+def sign(payload, is_access_token): #secret es la clave secreta
+    expiration = datetime.datetime.now(datetime.timezone.utc) + datetime.timedelta(seconds=10)  # Token expira en 10 segundos
+    return jwt.encode({"some": payload, "exp": expiration},
+                      "access" if is_access_token else "refresh",
+                      algorithm="HS256")
 
 
 """
 Se define una función la cual se encarga de crear una firma jwt
-Recibe 
-Retorna un token o un error
+Recibe el token a decodificar
+Retorna la carga tokenizada o un error de expiración
 """
-def sign(payload): #secret es la clave secreta
-    return jwt.encode({"some": payload}, "secret", algorithm="HS256", expires_in=3600)
-
-
-"""
-Se define una función la cual se encarga de crear una firma jwt
-Recibe 
-Retorna un token o un error
-"""
-def decode(encoded_jwt): #secret es la clave secreta
-    return jwt.decode(encoded_jwt, "secret", algorithms=["HS256"])
-
+def decode(encoded_jwt, is_access_token): #secret es la clave secreta
+    try:
+        return jwt.decode(encoded_jwt,
+                          "access" if is_access_token else "refresh",
+                          algorithms=["HS256"])
+    
+    except jwt.ExpiredSignatureError:
+        return "Token has expired"
 
 
 """
@@ -26,7 +34,7 @@ Recibe
 Retorna un token o un error
 """
 def create_access_token(doc):
-    pass
+    return sign(doc, True)
 
 """
 Se define una función la cual se encarga de crear un token de recarga
@@ -34,4 +42,4 @@ Recibe
 Retorna un token o un error
 """
 def create_refresh_token(doc):
-    pass
+    return sign(doc, False)
